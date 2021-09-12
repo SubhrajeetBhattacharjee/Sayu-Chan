94df1807fa3d7568bb46810d3ac63b3f0d867a41
Cheesecake <89353631+Cheesecake04@users.noreply.github.com>
2021-09-12 10:45:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const { token } = require("./config.json");
 const { Intents, Client, Collection } = require("discord.js"); 
-const mongoose = require('mongoose');
+
 const client = new Client({
   disableEveryone: true ,
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES]
 });
-mongoose.connect('mongodb+srv://Subhrajeet:slok3188@discordbot.grgnl.mongodb.net/test' , {useNewUrlParser: true, useUnifiedTopology: true})
-
 client.commands = new Collection();
 client.aliases = new Collection();
 
@@ -16,4 +14,3 @@ client.aliases = new Collection();
 
 
 client.login(token);
-/*mongodb+srv://Subhrajeet:<password>@discordbot.grgnl.mongodb.net/test*/
